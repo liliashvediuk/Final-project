31dc2f06254244ca19dbc09e77f4bebc70f368a7
liliashvediuk <lilasveduk9@gmail.com>
2026-05-30 22:09:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("subscribeForm");
+  const emailInput = document.getElementById("emailInput");
+  const formMessage = document.getElementById("formMessage");
+
+  form.addEventListener("submit", (event) => {
+    // Скасовуємо стандартне перезавантаження сторінки
+    event.preventDefault();
+
+    const emailValue = emailInput.value.trim();
+
+    // Проста валідація на вміст символу @
+    if (emailValue.includes("@")) {
+      formMessage.style.color = "green";
+      formMessage.textContent = `Дякуємо! На адресу ${emailValue} успішно оформлено підписку.`;
+
+      // Очищаємо поле введення
+      emailInput.value = "";
+    } else {
+      formMessage.style.color = "red";
+      formMessage.textContent =
+        "Будь ласка, введіть коректну електронну адресу.";
+    }
+  });
+});
